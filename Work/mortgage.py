4cba55a9bec71857313d6e3c4f257fa17c198fd1
Kwadwo Nyame Owusu-Boakye <knowusuboaky@mun.ca>
2020-07-12 11:16:49 +0000

    Assignment 4 completed

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,30 @@
 # mortgage.py
 #
 # Exercise 1.7
+# mortgage.py
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+
+extra_payment_start_month = int(input('Enter extra payment start month: '))
+extra_payment_end_month =  int(input ('Enter extra payment end month: '))
+extra_payment = float(input('Extra payment: '))
+month = 0
+
+while principal > 0:
+    month = month + 1;
+    if principal <= payment:
+        total_paid = total_paid + principal
+        principal = 0;
+    if not (principal <= payment):
+        if (month >= extra_payment_start_month and month <= extra_payment_end_month):
+            principal = principal * (1+rate/12) - (payment+extra_payment)
+            total_paid = total_paid + (payment+extra_payment)
+        if not (month >= extra_payment_start_month and month <= extra_payment_end_month):
+            principal = principal * (1+rate/12) - payment
+            total_paid = total_paid + payment
+    print(f'{month} {total_paid:0.2f} {principal:0.2f}')
+print(f'Total paid: {total_paid:0.2f}')
+print(f'Month     : {month}')
--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,30 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv
+import sys
+
+def portfolio_cost(filename):
+    f = open(filename, 'rt')
+    rows = csv.reader(f)
+    header = next(rows)
+    cost = 0;
+
+    for row in rows:
+        try:
+            shares = int(row[1])
+            value = float(row[2])
+            cost += shares * value
+        except ValueError:
+             print('Bad row encountered', row)
+    f.close()
+    return cost
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
+print('Total cost:', cost)
+
